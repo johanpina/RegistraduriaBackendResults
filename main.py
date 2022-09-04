f5ff0c1ac9924be3a7a5f3b559967bd1a7a8dcd1
RubenCameroAmador <37782994+RubenCameroAmador@users.noreply.github.com>
2022-09-04 15:39:15 -0500

    Creación del proyecto y modelado de clases completado

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_cors import CORS
+import json
+from waitress import serve
+
+
+app = Flask(__name__)
+cors = CORS(app)
+
+
+def loadFileConfig():
+    with open('config.json') as f:
+        data = json.load(f)
+    return data
+
+
+@app.route("/", methods=['GET'])
+def test():
+    json = {}
+    json["message"] = "Server running ..."
+    return jsonify(json)
+
+
+if __name__ == '__main__':
+    dataConfig = loadFileConfig()
+    print("Dataconfig:", dataConfig)
+    print("Server running : "+"http://"+dataConfig["url-backend"]+":" + str(dataConfig["port"]))
+    serve(app,host=dataConfig["url-backend"],port=dataConfig["port"])
